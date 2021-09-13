9115265caa7e80ca177ba508a3163691e2100b5c
poojaranidey <poojaranidey@gmail.com>
2021-09-13 19:05:35 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -15,16 +15,44 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
+      <p>Rating: ${product.rating.rate} (Total people rate:${product.rating.count})</p>
       <h2>Price: $ ${product.price}</h2>
-      <h3>Rating: ${product.rating.rate} <span style="margin-left: 8px; font-size: 15px;">(Total People Rate: ${product.rating.count})</span></h3>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success"><i class="fa fa-shopping-cart"> </i>  Add to cart</button>
+      <button onClick="randerSingleProduct(${product.id})" id="details-btn" class="btn btn-danger"><i class="fa fa-info-circle"> </i> Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+//rander single product
+const randerSingleProduct = (id) => {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showDetails(data));
+  // console.log(id);
+};
+
+const showDetails = (data) => {
+  // console.log(data);
+  document.getElementById('show-details-card').innerHTML = '';
+  const div = document.createElement("div");
+  div.classList.add("product");
+  div.innerHTML = `<div class="details-card">
+      <div>
+        <img class="product-image" src=${data.image}></img>
+      </div>
+      <h3>${data.title}</h3>
+      <h6>Price: ${data.price}</h6>
+      <p>Rating: ${data.rating.rate} (${data.rating.count})</p>
+      <p><small>${data.description}</small></p>
+      
+      `;
+  document.getElementById("show-details-card").appendChild(div);
+};
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -77,4 +105,9 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+// buy now button click (order confirmation)
+const placeOrder = () => {
+  alert("Your order placed. Thank you.")
+}
 loadProducts();
